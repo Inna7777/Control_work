c1e9704a59a32d80711a871bffc8cab5efdacc6a
Inna7777 <peshkova-inna@mail.ru>
2023-01-27 18:55:26 +0400

    Написан  методсоздания нового массива из заданного

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] arr1 = {"Helloy", "2","world", ":-)"};
+string[] arr2 = {"1234", "1567", "-2","computer science"};
+string[] arr3 = {"Russia", "Denmark", "Kazan"}; // Заданные массивы.
+
+    string [] ShortsArray (string[] array)// Функция создания нового массива из элементов <=3
+    {
+        int length = array.Length;
+        string [] result = new string[length];
+        int count = 0;
+        for (int i = 0; i < length; i++)
+        {
+            if (array[i].Length <=3)
+            {
+                result[count] = array[i];
+                count ++;
+            }
+            Array.Resize(ref result, count);//Создаем новый массив. В массиве count елементов.
+        }
+        return result;
+    }
+    
