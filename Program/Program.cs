0602a5dec83e08738f40f2ca4dccf4b7491d69f2
Inna7777 <peshkova-inna@mail.ru>
2023-01-27 21:10:25 +0400

    Форматирование кода

--- Program/Program.cs
+++ Program/Program.cs
@@ -2,20 +2,20 @@
 string[] arr2 = {"1234", "1567", "-2","computer science"};
 string[] arr3 = {"Russia", "Denmark", "Kazan"}; // Заданные массивы.
 
-    string [] ShortsArray (string[] array)  // Функция создания нового массива из элементов <=3
+    string[] ShortsArray(string[] array)  // Функция создания нового массива из элементов <=3
     {
         int length = array.Length;
-        string [] result = new string[length];
+        string[] result = new string[length];
         int count = 0;
         for (int i = 0; i < length; i++)
         {
             if (array[i].Length <= 3)
             {
                 result[count] = array[i];
-                count ++;
+                count++;
             }
-            Array.Resize(ref result, count);//Создаем новый массив. В массиве count елементов.
         }
+         Array.Resize(ref result, count);//Создаем новый массив. В массиве count елементов.
         return result;
     }
     void PrintNewArray(string[] newArray) //Функция вывода полученного нового массива
@@ -40,7 +40,7 @@ void PrintArr(string[] array)
     Console.WriteLine();
 }
 
-PrintArr (arr1);
-PrintArr (arr2);
-PrintArr (arr3);
+PrintArr(arr1);
+PrintArr(arr2);
+PrintArr(arr3);
 
